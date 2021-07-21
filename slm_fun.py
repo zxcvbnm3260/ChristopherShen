82a51515c4486c750cbf929769753120b4e4716a
zxcvbnm3260 <35607166+zxcvbnm3260@users.noreply.github.com>
2021-07-21 11:13:57 +0800

    Add files via upload

--- slm_fun.py
+++ slm_fun.py
@@ -147,8 +147,7 @@ def slm_fmz_df2table(df):
 
 #%%% 3.停止按钮结束循环
 
-def slm_fmz_stop_button(button_name='停止机器人'):
-    cmd = eval(' GetCommand()')
+def slm_fmz_stop_button(cmd, button_name='停止机器人'):
     if cmd==button_name: 
         Log('收到指令：停止机器人！')
         eval('break')
